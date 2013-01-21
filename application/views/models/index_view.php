72ede6771f361c0ec528cce12ba2a5791546274a
Mr.Satit <rianpit@gmail.com>
2013-01-21 08:38:50 +0700

    update new module

--- application/views/models/index_view.php
+++ application/views/models/index_view.php
@@ -52,7 +52,7 @@
 
 <div class="modal hide fade" id="mdl_new">
     <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
         <h3>เพิ่มรายการ</h3>
     </div>
     <div class="modal-body">
